138802cf3773063bf5378590d25d909b341db05d
Juliano Leal Goncalves <jgoncalves@affinitiv.com>
2024-12-06 20:35:22 -0300

    Create sample.

---
+++ Project1/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Project1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Project2/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Project2
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
